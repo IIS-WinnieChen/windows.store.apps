fd65ef3ccfca864397e56fccaf366b318c0b5a28
Ian Hong <ianhong@outlook.com>
2013-04-18 13:29:09 -0700

    After new nuspec

--- ContosoCookBookPortableLibrary/Properties/AssemblyInfo.cs
+++ ContosoCookBookPortableLibrary/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ContosoCookBookPortableLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ContosoCookBook PortableLibrary")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corp.")]
 [assembly: AssemblyProduct("ContosoCookBookPortableLibrary")]
